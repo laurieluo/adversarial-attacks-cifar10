9ea236bff338dd77bc8dcc92118e744300c1099e
Laurie Luo <laurieluo2003@gmail.com>
2025-11-04 18:47:53 +0800

    Implement training script for CIFAR-10 with ResNet18, VGG16, and DenseNet121 models

--- src/models.py
+++ src/models.py
@@ -1,15 +1,15 @@
 # src/models.py
-
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import re
+from collections import OrderedDict
 
 # ----------------------------------------------------------------------
-# ResNet18 Model Definition (suitable for CIFAR-10)
+# 1. ResNet18 
 # ----------------------------------------------------------------------
-
 class BasicBlock(nn.Module):
     expansion = 1
-
     def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(
@@ -18,7 +18,6 @@ class BasicBlock(nn.Module):
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
                                stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
-
         self.downsample = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
             self.downsample = nn.Sequential(
@@ -26,7 +25,6 @@ class BasicBlock(nn.Module):
                           kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(self.expansion*planes)
             )
-
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
@@ -38,7 +36,6 @@ class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=10):
         super(ResNet, self).__init__()
         self.in_planes = 64
-
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
                                stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
@@ -46,9 +43,7 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        
-        self.fc = nn.Linear(512*block.expansion, num_classes)
-
+        self.fc = nn.Linear(512*block.expansion, num_classes) # 使用 'fc'
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
         layers = []
@@ -56,7 +51,6 @@ class ResNet(nn.Module):
             layers.append(block(self.in_planes, planes, stride))
             self.in_planes = planes * block.expansion
         return nn.Sequential(*layers)
-
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.layer1(out)
@@ -65,10 +59,116 @@ class ResNet(nn.Module):
         out = self.layer4(out)
         out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
-        
         out = self.fc(out)
-        
         return out
 
 def ResNet18():
     return ResNet(BasicBlock, [2, 2, 2, 2])
+
+# ----------------------------------------------------------------------
+# 2. VGG16_BN 
+# ----------------------------------------------------------------------
+cfg = {
+    'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+}
+class VGG(nn.Module):
+    def __init__(self, vgg_name):
+        super(VGG, self).__init__()
+        self.features = self._make_layers(cfg[vgg_name])
+        # CIFAR-10: VGG16 在 32x32 图像上的输出是 1x1x512
+        self.classifier = nn.Linear(512, 10)
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        for x in cfg:
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
+                           nn.BatchNorm2d(x),
+                           nn.ReLU(inplace=True)]
+                in_channels = x
+        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+        return nn.Sequential(*layers)
+
+def VGG16_BN():
+    return VGG('VGG16')
+
+# ----------------------------------------------------------------------
+# 3. DenseNet121 
+# ----------------------------------------------------------------------
+class _DenseLayer(nn.Module):
+    def __init__(self, num_input_features, growth_rate, bn_size, drop_rate):
+        super().__init__()
+        self.norm1 = nn.BatchNorm2d(num_input_features)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv1 = nn.Conv2d(num_input_features, bn_size * growth_rate, kernel_size=1, stride=1, bias=False)
+        self.norm2 = nn.BatchNorm2d(bn_size * growth_rate)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(bn_size * growth_rate, growth_rate, kernel_size=3, stride=1, padding=1, bias=False)
+        self.drop_rate = float(drop_rate)
+    def forward(self, features):
+        if isinstance(features, torch.Tensor):
+            features = [features]
+        prev_features = torch.cat(features, 1)
+        new_features = self.conv1(self.relu1(self.norm1(prev_features)))
+        new_features = self.conv2(self.relu2(self.norm2(new_features)))
+        if self.drop_rate > 0:
+            new_features = F.dropout(new_features, p=self.drop_rate, training=self.training)
+        return new_features
+
+class _DenseBlock(nn.ModuleDict):
+    def __init__(self, num_layers, num_input_features, bn_size, growth_rate, drop_rate):
+        super().__init__()
+        for i in range(num_layers):
+            layer = _DenseLayer(num_input_features + i * growth_rate, growth_rate, bn_size, drop_rate)
+            self.add_module("denselayer%d" % (i + 1), layer)
+    def forward(self, init_features):
+        features = [init_features]
+        for name, layer in self.items():
+            new_features = layer(features)
+            features.append(new_features)
+        return torch.cat(features, 1)
+
+class _Transition(nn.Sequential):
+    def __init__(self, num_input_features, num_output_features):
+        super().__init__()
+        self.norm = nn.BatchNorm2d(num_input_features)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv = nn.Conv2d(num_input_features, num_output_features, kernel_size=1, stride=1, bias=False)
+        self.pool = nn.AvgPool2d(kernel_size=2, stride=2)
+
+class DenseNet(nn.Module):
+    def __init__(self, growth_rate=32, block_config=(6, 12, 24, 16), num_init_features=64, 
+                 bn_size=4, drop_rate=0, num_classes=10):
+        super().__init__()
+        # CIFAR-10: 初始 conv 层修改
+        self.features = nn.Sequential(OrderedDict([
+            ('conv0', nn.Conv2d(3, num_init_features, kernel_size=3, stride=1, padding=1, bias=False)),
+        ]))
+        num_features = num_init_features
+        for i, num_layers in enumerate(block_config):
+            block = _DenseBlock(num_layers, num_features, bn_size, growth_rate, drop_rate)
+            self.features.add_module("denseblock%d" % (i + 1), block)
+            num_features += num_layers * growth_rate
+            if i != len(block_config) - 1:
+                trans = _Transition(num_features, num_features // 2)
+                self.features.add_module("transition%d" % (i + 1), trans)
+                num_features = num_features // 2
+        self.features.add_module("norm5", nn.BatchNorm2d(num_features))
+        self.classifier = nn.Linear(num_features, num_classes)
+    def forward(self, x):
+        features = self.features(x)
+        out = F.relu(features, inplace=True)
+        out = F.adaptive_avg_pool2d(out, (1, 1))
+        out = torch.flatten(out, 1)
+        out = self.classifier(out)
+        return out
+
+def DenseNet121():
+    return DenseNet(growth_rate=32, block_config=(6, 12, 24, 16), num_init_features=64)
